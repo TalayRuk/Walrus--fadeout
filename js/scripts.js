b6a7ac612e475bb477036b4a5a2b20d121fcbe0a
Ian Wilcox and Vichitra Pool <ian+vichitra@>
2016-08-09 11:27:32 -0700

    add clickable text

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,24 @@ $(document).ready(function() {
     $("#initial-showing").toggle();
     $("#initial-hidden").toggle();
   });
+
   $(".clickable2").click(function() {
     $("#fadeincat").fadeToggle("slow");
     $("#fadeoutcat").fadeToggle();
   });
+
   $(".clickable3").click(function() {
     $("#dog").slideUp();
   });
   $(".clickable3").click(function() {
     $("#dog").show();
   });
+
+  $(".clickable4").click(function() {
+    $("#text-showing").fadeIn();
+  });
+  $(".clickable4").click(function() {
+    $("#text-showing").hide();
+  });
+
 });
